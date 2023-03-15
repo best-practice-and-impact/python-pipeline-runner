8d7da3e6157f3f2ad738e9e52a4804ae12309b0a
David Foster <foster.dev999@gmail.com>
2023-03-15 14:33:31 +0000

    Fix function reference in native example

--- native_python_example/pipeline.py
+++ native_python_example/pipeline.py
@@ -6,12 +6,12 @@ import pandas as pd
 # Could instead references them in the steps using df.column_name to make it clear which input they are from
 from input_file import column_1, column_2
 
-from example_module import add_scalar_to_series
+from example_module import add_scalar_to_series, sum_series
 from constants import SOME_SCALAR
 
 # Although the methods are trivial, this approach keeps the logic defined separately and testable
 # It also doesn't care if these are series, scalar values, numpy arrays etc
-column_3 = sum(column_1, column_2)
+column_3 = sum_series(column_1, column_2)
 column_4 = add_scalar_to_series(column_3, SOME_SCALAR)
 
 # Would move this to a runner that uses getattr and executes the pipeline
